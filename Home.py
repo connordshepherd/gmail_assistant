017e829f7c037394daed21f8432ebe42819e130a
Connor Shepherd <31867826+connordshepherd@users.noreply.github.com>
2024-01-04 21:41:46 -0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.title("Gmail Assistant")
 
-st.info("Enter some Google Login tokens into the Build Gmail Service page on the left sidebar. Then test it on the next page, and on the last page you can chat with your email.")
+st.info("Enter some Google Login tokens into the Build Gmail Service page on the left sidebar. Then test it on the next page, and on the last page you can chat with your email. Nothing is stored anywwhere.")
