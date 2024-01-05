b6bdc057e2be185df90c040cc62019c58750c74a
Connor Shepherd <31867826+connordshepherd@users.noreply.github.com>
2024-01-04 21:41:27 -0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,3 +1,5 @@
 import streamlit as st
 
 st.title("Gmail Assistant")
+
+st.info("Enter some Google Login tokens into the Build Gmail Service page on the left sidebar. Then test it on the next page, and on the last page you can chat with your email.")
